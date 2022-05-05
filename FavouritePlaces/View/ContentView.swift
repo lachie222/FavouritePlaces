1a2a369708dacf1a53605e5dbf9b913b81e6ee4f
Lachlan Manson <lachlan.manson@griffithuni.edu.au>
2022-05-05 16:22:34 +1000

    Separated CRUD functions

---
+++ FavouritePlaces/View/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  FavouritePlaces
+//
+//  Created by Lachlan Manson on 4/5/2022.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Place.name, ascending: true)],
+        animation: .default)
+    var places: FetchedResults<Place>
+    
+    var body: some View {
+        NavigationView {
+            PlaceListView()
+                .navigationTitle("Favourite Places")
+                .navigationBarItems(leading: EditButton(), trailing: Button(action: {
+                    addPlace(context: viewContext)
+                }, label: {Image(systemName: "plus")}))
+            }
+            Text("Select an item")
+        }
+}
+
+
+
+
+
+/*struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}*/
