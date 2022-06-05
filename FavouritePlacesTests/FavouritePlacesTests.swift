63555e4da54629c549a42aad1433fe04e7a3174b
Lachlan Manson <lachlan.manson@griffithuni.edu.au>
2022-06-05 12:55:23 +1000

    Tests written

--- FavouritePlacesTests/FavouritePlacesTests.swift
+++ FavouritePlacesTests/FavouritePlacesTests.swift
@@ -18,14 +18,24 @@ class FavouritePlacesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testPlace() throws {
+        let context = PersistenceController.init().container.viewContext
+        let newPlace = Place(context: context)
+        newPlace.name = "Brisbane"
+        XCTAssertEqual(newPlace.name, "Brisbane")
     }
-
+    
+    func testSunriseSunsetModel() throws {
+        let context = PersistenceController.init().container.viewContext
+        let newPlace = Place(context: context)
+        newPlace.name = "Brisbane"
+        newPlace.lookupLocation(for: newPlace.placeName)
+        newPlace.lookupSunriseAndSunset()
+        XCTAssertNotEqual(newPlace.sunrise, "Unknown")
+        XCTAssertNotEqual(newPlace.sunrise, nil)
+        
+    }
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
